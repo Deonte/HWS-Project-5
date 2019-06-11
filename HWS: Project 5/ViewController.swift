34b6465a2412ed8dab8a554b5981ca30fbdc0881
Deonte <dkill51@gmail.com>
2019-06-11 17:21:58 -0400

    Project 5 Challenge Complete. 1. Disallow answers that are shorter than three letters or are just our start word. For the three-letter check, the easiest thing to do is put a check into isReal() that returns false if the word length is under three letters. For the second part, just compare the start word against their input word and return false if they are the same. 2. Refactor all the else statements we just added so that they call a new method called showErrorMessage(). This should accept an error message and a title, and do all the UIAlertController work from there. 3. Add a left bar button item that calls startGame(), so users can restart with a new word whenever they want to. Bonus Challenge: Once you’ve done those three, there’s a really subtle bug in our game and I’d like you to try finding and fixing it.”

--- HWS: Project 5/ViewController.swift
+++ HWS: Project 5/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        //Challenge 3: Create a new left bar button item that calls startGame()
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(startGame))
         
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startwords = try? String(contentsOf: startWordsURL) {
@@ -32,7 +34,7 @@ class ViewController: UITableViewController {
         startGame()
     }
     
-    func startGame() {
+    @objc func startGame() {
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
@@ -43,7 +45,8 @@ class ViewController: UITableViewController {
         ac.addTextField()
         
         let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] _ in
-            guard let answer = ac?.textFields?[0].text else {return}
+            //Bug fix: Bug answer would allow the user to enter the same word twice if entered first in Uppercase then once again in lowercase. My Solution was to only acept answers in their lowercased form.
+            guard let answer = ac?.textFields?[0].text?.lowercased() else {return}
             self?.submit(answer)
         }
         
@@ -53,30 +56,39 @@ class ViewController: UITableViewController {
     
     func submit(_ answer: String) {
         let lowerAnswer = answer.lowercased()
-        let errorTitle: String
-        let errorMessage: String
         
-        if isPossible(word: lowerAnswer) {
-            if isOriginal(word: lowerAnswer) {
-                if isReal(word: lowerAnswer) {
-                    usedWords.insert(answer, at: 0)
-                    
-                    let indexPath = IndexPath(row: 0, section: 0)
-                    tableView.insertRows(at: [indexPath], with: .automatic)
-                    return
+        // Challenge 1a: Dissallow use of the start word.
+        if lowerAnswer != title {
+            // Challenge 1b: Dissallow the use of words less than 3 characters
+            if lowerAnswer.count >= 3 {
+            if isPossible(word: lowerAnswer) {
+                if isOriginal(word: lowerAnswer) {
+                    if isReal(word: lowerAnswer) {
+                        usedWords.insert(answer, at: 0)
+                        
+                        let indexPath = IndexPath(row: 0, section: 0)
+                        tableView.insertRows(at: [indexPath], with: .automatic)
+                        return
+                    } else {
+                        showErrorMessage(errorTitle: "Word not recognised", errorMessage: "YoU CaN'T jUsT MakE theM Up YoU KnoW!")
+                    }
                 } else {
-                    errorTitle = "Word not recognised"
-                    errorMessage = "YoU CaN'T jUsT MakE theM Up YoU KnoW!"
+                    showErrorMessage(errorTitle: "Word Used Already", errorMessage: "Be more original!")
                 }
             } else {
-                errorTitle = "Word Used Already"
-                errorMessage = "Be more original!"
+                showErrorMessage(errorTitle: "Word not possible", errorMessage: "You cant spell that word from \(title!.lowercased())")
+                }
+            } else {
+                showErrorMessage(errorTitle: "Need 3 or more letters", errorMessage: "Can't use less than 3 letters.")
             }
         } else {
-            errorTitle = "Word not possible"
-            errorMessage = "You cant spell that word from \(title!.lowercased())"
+            showErrorMessage(errorTitle: "No, no, no", errorMessage: "You can't just use the same word.")
         }
         
+    }
+    
+    // Challenge 2: Refactor all the else statements so that they call a new method called showErrorMessage()
+    func showErrorMessage(errorTitle: String, errorMessage: String) {
         let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
         present(ac, animated:  true)
